4f121ab29f97e060ee03942b845ce522d1d47e75
Relentless <78308671+penpinz@users.noreply.github.com>
2024-05-12 22:43:02 +0530

    ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-38d33a7f-f648-4e74-a01c-69fa944502d2/"
 
 OUTPUT_DIR="logs"
 
